4e550ab6a0afacbe380d9806c22dce51ea13b75b
tesla1618 <rajiebbb@gmail.com>
2026-03-30 18:41:16 +0600

    next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 // next.config.js
 module.exports = {
-  allowedDevOrigins: ["192.168.0.113"],
+  allowedDevOrigins: ["192.168.0.113"], // this is my local IP address, and I have no problem with it being public. You can even publish it to a tech news website calling me a "dumbass".
 };
