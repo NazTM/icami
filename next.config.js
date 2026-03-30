92340f530b15f9db21572dd63ed73132e77c431a
tesla1618 <rajiebbb@gmail.com>
2026-03-30 08:03:22 +0600

    fuck github

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-// // next.config.js
-// module.exports = {
-//   allowedDevOrigins: ["192.168.0.113"],
-//   output: "export",
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-
-  images: {
-    unoptimized: true,
-  },
-
-  trailingSlash: true,
+// next.config.js
+module.exports = {
+  allowedDevOrigins: ["192.168.0.113"],
 };
-
-module.exports = nextConfig;
