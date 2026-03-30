31821a7fe1cac9071264300c27adb1964b15c414
tesla1618 <rajiebbb@gmail.com>
2026-03-30 06:48:35 +0600

    feat(config): update Next.js configuration with export output, unoptimized images, and trailing slash support.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-// next.config.js
-module.exports = {
-  allowedDevOrigins: ["192.168.0.113"],
+// // next.config.js
+// module.exports = {
+//   allowedDevOrigins: ["192.168.0.113"],
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// /** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
+
+module.exports = nextConfig;
