e8dc4081f613baa8a740fad0c107789d14069ba9
tesla1618 <rajiebbb@gmail.com>
2026-03-31 12:55:37 +0600

    forgot honestly

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
-  allowedDevOrigins: ["192.168.0.113"], // this is my local IP address, and I have no problem with it being public. You can even publish it to a tech news website calling me a "dumbass".
+  allowedDevOrigins: ["192.168.0.114"], // this is my local IP address, and I have no problem with it being public. You can even publish it to a tech news website calling me a "dumbass".
   images: {
     remotePatterns: [
       {
